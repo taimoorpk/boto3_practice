276cdcccf9a4b1f917d17e87445ba2a6c6b91ac9
taimoorpk <kh.taimoor@gmail.com>
2025-05-25 09:20:00 +0100

    file1 and file2 contents read

--- s3_crud.py
+++ s3_crud.py
@@ -20,3 +20,21 @@ else:
 for bucket in s3.buckets.all():
     print(bucket.name)
 
+#create file1 and file2
+file1 = "file1.txt"
+file2 = "file2.txt"
+
+# upload file1 to bucket
+s3.Bucket(bucket_name).upload_file(Filename=file1,Key=file1)
+
+# Read the file content
+
+obj = s3.Object(bucket_name,file1)
+body = obj.get()["Body"].read()
+print(body)
+
+# Update fil1 in the bucket with new content from file2
+s3.Object(bucket_name,file1).put(Body=open(file2,'rb'))
+
+body = s3.Object(bucket_name,file1).get()["Body"].read()
+print(body)
